ad60cc31d8dbcb3811a9744f91dabd83996aa354
jessicaappeldoorn <jessica.appeldoorn@gmail.com>
2015-06-15 22:22:56 -0700

    timer ding is functional

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,31 +26,18 @@ var app = angular.module("Pulseandpause", ["firebase", "ui.router"]);
 // home controller
 app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$timeout', function($scope, $firebaseArray, $interval, $timeout){
   
-  var ref = new Firebase("https://pulseandpause.firebaseio.com");
-
-
-  $scope.tasks = $firebaseArray(ref);
-
-  var fireTime = Firebase.ServerValue.TIMESTAMP;
-  var timeEnd,
+  var ref = new Firebase("https://pulseandpause.firebaseio.com"),
+  fireTime = Firebase.ServerValue.TIMESTAMP,
+  timeEnd,
   counter,
-  time,
-  count;
-
-
-  var mySound = new buzz.sound("http://soundjax.com/reddo/56895%5EDING.mp3", {
+  time, 
+  count,
+  mySound = new buzz.sound("http://soundjax.com/reddo/56895%5EDING.mp3", {
     preload: true
   });
 
-  console.log("this is a message about allTasks" + " " + $scope.allTasks);
-
+  $scope.tasks = $firebaseArray(ref);
   $scope.list = [];
-  $scope.submit = function() {
-    if ($scope.newTaskText) {
-      $scope.list.push(this.newTaskText);
-    }
-  };
-
   $scope.timer = {
     text: undefined,
     date: new Date (),
@@ -59,66 +46,75 @@ app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$ti
     onBreak: false,
     working: false,
     session: 0,
-    created: undefined
+    created: undefined,
+    workSound: false,
+    breakSound: false
   };
 
+  $scope.submit = function() {
+    if ($scope.newTaskText) {
+      $scope.list.push(this.newTaskText);
+    }
+  };
 
-  $scope.addTask = function() { // add task to history list
-    var newTask = {
-    text: $scope.newTaskText,
-    created: fireTime,
-  }
-    
+  $scope.$watch('timer.breakSound', function(newValue, oldValue) {
+    if(newValue) {
+      mySound.play();
+    }
+  }); 
 
+  $scope.$watch('timer.workSound', function(newValue, oldValue) {
+      if(newValue) {
+      mySound.play();
+    }
+  });
+
+  $scope.createTask = function() {
+    $scope.newTask = {
+      name: $scope.newTaskText,
+      created: fireTime,
+      session: 1
+    };
+
+        if($scope.newTask.name === undefined) {
+        $scope.newTask.name = null;
+    };
     $scope.submit();
-
-    $scope.tasks.$add(newTask); // Push into array
+    $scope.tasks.$add($scope.newTask);
     $scope.newTaskText = "";
-  };
-
-    $scope.$watch('timer.timer', function(newValue, oldValue) {
-      console.log("We have a newValue");
-              mySound.play();
-
-      if(newValue < 250) {
-        mySound.play();
-        console.log("doing something");
-      }
-    }); 
-
-  /*  $scope.$watch(function(scope) { 
-      return scope.timer.timer }, function(newValue, oldValue) {
-        if(newValue < 250) {
-      mySound.play(); 
-      console.log("supposed to do something here"); };
-  }); */
-  // $scope.tasks = $firebaseArray(ref);
-  //timeEnd = 0;
+  }
 
   $scope.startWorkTimer = function() {
-    console.log('started work');
-
     $interval.cancel(counter);
     timeEnd = " ";
 
-    var newSession = {
-      name: $scope.newTaskText,
-      created: fireTime
-    };
+    
+    $scope.createTask();
 
-    $scope.submit();
+  
     
 
-    $scope.tasks.$add(newSession);
-    $scope.newTaskText = "";
+    // var newSession = {
+    //   name: $scope.newTaskText,
+    //   created: fireTime,
+    //   session: 1
+    // };
 
+    // $scope.submit();
+    
 
+    // $scope.tasks.$add(newTask);
+    // $scope.newTaskText = "";
+
+    $scope.timer.text = $scope.newTaskText;
     $scope.timer.mode = "Stop";
     $scope.timer.timer = timeEnd;
     $scope.timer.working = true;
     $scope.timer.onBreak = false;
     $scope.timer.session += 1;
+    $scope.timer.workSound = false;
     
+
     var timeStart = undefined,
     time = undefined;
     timeEnd = new Date().setMilliseconds(7000); //1502000
@@ -131,8 +127,8 @@ app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$ti
         $scope.timer.timer = time;
 
         if (time < 250) {
+          $scope.timer.workSound = true;
           $scope.startBreakTimer();
-          mySound.play();
         }};
     }, 1000);  
   };
@@ -146,6 +142,8 @@ app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$ti
     $scope.timer.timer = "READY";
     $scope.timer.onBreak = true;
     $scope.timer.working = false;
+    $scope.timer.breakSound = false;
+    
 
     var timeStart = undefined;
     var time = undefined;
@@ -169,7 +167,8 @@ app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$ti
         $scope.timer.timer = time;
 
         if (time < 250){
-          $scope.startWorkTimer();
+          $scope.timer.breakSound = true;
+          //$scope.startWorkTimer();
         };
       }
     }, 1000); 
@@ -193,6 +192,7 @@ app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$ti
     $scope.timer.onBreak = false,
     $scope.timer.working = false,
     $scope.timer.session = 0
+    $scope.list.pop(this.newTaskText);
   };
 
 
