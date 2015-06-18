a0d7219d403995db17c8a5f7be72612475166657
jessicaappeldoorn <jessica.appeldoorn@gmail.com>
2015-06-17 18:29:06 -0700

    WIP on dashboard

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,7 +12,7 @@ var app = angular.module("Pulseandpause", ["firebase", "ui.router"]);
 
    $stateProvider.state('dashboard', {
      url: '/dashboard',
-     controller: 'Home.controller',
+     controller: 'Dashboard.controller',
      templateUrl: '/templates/dashboard.html'
    });
 
@@ -33,7 +33,7 @@ app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$ti
   counter,
   time, 
   count,
-  mySound = new buzz.sound("http://soundbible.com/grab.php?id=1746&type=mp3", {
+  mySound = new buzz.sound("http://soundjax.com/reddo/56895%5EDING.mp3", {
     preload: true
   });
 
@@ -71,15 +71,17 @@ app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$ti
   });
 
   $scope.createTask = function() {
+    // check if newTaskText  is undefined
+    // if ($scope.newTaskText === undefined) { 
+    //   $scope.newTaskText = null; 
+    // };
+
     $scope.newTask = {
       name: $scope.newTaskText,
       created: fireTime,
       session: 1
     };
 
-        if($scope.newTask.name === undefined) {
-        $scope.newTask.name = null;
-    };
     $scope.submit();
     $scope.tasks.$add($scope.newTask);
     $scope.newTaskText = "";
@@ -89,23 +91,12 @@ app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$ti
     $interval.cancel(counter);
     timeEnd = " ";
 
-    
-    $scope.createTask();
-
-  
-    
-
-    // var newSession = {
-    //   name: $scope.newTaskText,
-    //   created: fireTime,
-    //   session: 1
-    // };
-
-    // $scope.submit();
-    
-
-    // $scope.tasks.$add(newTask);
-    // $scope.newTaskText = "";
+    if($scope.newTask === undefined) {
+      $scope.createTask();
+    } else {
+      $scope.newTask.session += 1;
+      console.log($scope.newTask.session + $scope.newTask.name);
+    };
 
     $scope.timer.text = $scope.newTaskText;
     $scope.timer.mode = "Stop";
@@ -194,6 +185,7 @@ app.controller('Home.controller', ['$scope', '$firebaseArray', '$interval', '$ti
     $scope.timer.working = false,
     $scope.timer.session = 0
     $scope.list.pop(this.newTaskText);
+     $scope.newTaskText = "";
   };
 
 
@@ -212,7 +204,68 @@ app.controller('Timer.controller', ['$scope', '$firebaseArray', function($scope,
 app.controller('Dashboard.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
   var ref = new Firebase("https://pulseandpause.firebaseio.com");
 // create a synchronized (psuedo read-only) array
+  var data = [
+    {
+        value: 20,
+        color:"#637b85"
+    },
+    {
+        value : 30,
+        color : "#2c9c69"
+    },
+    {
+        value : 40,
+        color : "#dbba34"
+    },
+    {
+        value : 10,
+        color : "#c62f29"
+    }
 
+];
+
+var canvas = document.getElementById("hours");
+var ctx = canvas.getContext("2d");
+new Chart(ctx).Doughnut(data);
+
+var data = {
+    labels : ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+    datasets : [
+        {
+            fillColor : "rgba(99,123,133,0.4)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [65,54,30,81,56,55,40]
+        },
+        {
+            fillColor : "rgba(219,186,52,0.4)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [20,60,42,58,31,21,50]
+        },
+    ]
+}
+var canvas = document.getElementById("shipments");
+var ctx = canvas.getContext("2d");
+new Chart(ctx).Line(data);
+
+var data = {
+    labels : ["Helpful","Friendly","Kind","Rude","Slow","Frustrating"],
+    datasets : [
+        {
+            fillColor : "rgba(220,220,220,0.5)",
+            strokeColor : "#637b85",
+            pointColor : "#dbba34",
+            pointStrokeColor : "#637b85",
+            data : [65,59,90,81,30,56]
+        }
+    ]
+}
+$scope.canvas = document.getElementById("departments");
+var ctx = $scope.canvas.getContext("2d");
+new Chart(ctx).Radar(data);
 
 
 }]);
@@ -220,8 +273,18 @@ app.controller('Dashboard.controller', ['$scope', '$firebaseArray', function($sc
 app.controller('Login.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
   var ref = new Firebase("https://pulseandpause.firebaseio.com");
 // create a synchronized (psuedo read-only) array
+  $scope.user = $firebaseArray(ref);
 
+  $scope.createUser = function() {
+    var newUser = {
+      email: $scope.userEmail,
+      password: $scope.userPassword
+    };
 
+    $scope.user.$add(newUser);
+    $scope.userEmail = "";
+    $scope.userPassword = "";
+  }
 
 }]);
 
@@ -231,25 +294,63 @@ app.directive('ngStopwatch', ['$interval', function($interval) {
     replace: true,
     controller: 'Home.controller',
     restrict: 'E'
-};
-
-
-   
-
-
-
+  };
 }]);
 
-/*app.factory('Tasks', ['$firebaseObject', '$firebaseArray', function($firebaseObject, $firebaseArray) {
+app.factory('taskRepository', ['$firebaseObject', '$firebaseArray', function($firebaseObject, $firebaseArray) {
 
   var ref = new Firebase("https://pulseandpause.firebaseio.com");
   var tasks = $firebaseArray(ref);
   //$scope.timer = $firebaseArray(ref);
   return {
     allTasks: tasks
-  }
-}]);*/
+  };
+}]);
 
+
+app.factory('timerFactory', function() {
+
+  var timer = {};
+  timer.newSession = function() { 
+
+    $interval.cancel(counter);
+    timeEnd = " ";
+
+    if($scope.newTask === undefined) {
+      $scope.createTask();
+    } else {
+      $scope.newTask.session += 1;
+      console.log($scope.newTask.session + $scope.newTask.name);
+    };
+
+    $scope.timer.text = $scope.newTaskText;
+    $scope.timer.mode = "Stop";
+    $scope.timer.timer = timeEnd;
+    $scope.timer.working = true;
+    $scope.timer.onBreak = false;
+    $scope.timer.session += 1;
+    $scope.timer.workSound = false;
+    
+
+    var timeStart = undefined,
+    time = undefined;
+    timeEnd = new Date().setMilliseconds(7000); //1502000
+    
+    counter = $interval(function(){ 
+      if($scope.timer.working === true){
+        timeStart = new Date().getTime();  
+        time = timeEnd - timeStart;
+
+        $scope.timer.timer = time;
+
+        if (time < 250) {
+          $scope.timer.workSound = true;
+          $scope.startBreakTimer();
+        }};
+    }, 1000);  
+  };
+
+});
 
 
 
